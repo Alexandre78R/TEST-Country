786e96f09f19cc74c1c6389f8c7e915dee326252
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-13 10:32:52 +0200

    add header

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import "@/styles/reset.css";
+import "@/components/Header/header.css";
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 
