177df61fb5f232691121a983c8b9cdc7e81e9912
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2024-05-13 12:18:52 +0200

    fix list countries

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/Header/Header";
-import Countries from "@/components/ListCountries/ListCountries";
+import ListCountries from "@/components/ListCountries/ListCountries";
 export default function Home() {
   return (
     <main>
       <Header />
-      <Countries />
+      <ListCountries />
     </main>
   );
 }
